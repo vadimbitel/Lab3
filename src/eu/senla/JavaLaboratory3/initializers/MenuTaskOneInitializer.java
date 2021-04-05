6e7a706fc2be95e7a70c53f0500be8e8e5b43bd1
Artemfrag <silantevartemf@gmail.com>
2021-04-05 23:49:23 +0300

    Add all menu items from Task 1

--- src/eu/senla/JavaLaboratory3/initializers/MenuTaskOneInitializer.java
+++ src/eu/senla/JavaLaboratory3/initializers/MenuTaskOneInitializer.java
@@ -1,5 +1,9 @@
 package eu.senla.JavaLaboratory3.initializers;
 
+import eu.senla.JavaLaboratory3.actions.DisplayDiffirenceBetweenNumbersAction;
+import eu.senla.JavaLaboratory3.actions.DisplayMaxDigitAction;
+import eu.senla.JavaLaboratory3.actions.DisplaySumOfDigitsAction;
+import eu.senla.JavaLaboratory3.actions.DisplaySumOfFirstDigitsAction;
 import eu.senla.JavaLaboratory3.menu.Menu;
 import eu.senla.JavaLaboratory3.menu.MenuContext;
 import eu.senla.JavaLaboratory3.menu.MenuItem;
@@ -11,6 +15,17 @@ public class MenuTaskOneInitializer {
         MenuContext.setCurrentMenu(new Menu());
         MenuContext.getCurrentMenu().setItems(new ArrayList<>());
 
+        MenuItem displayMaxDigit= new MenuItem("Display max digit from random number ", new DisplayMaxDigitAction());
+        MenuContext.getCurrentMenu().getItems().add(displayMaxDigit);
+
+        MenuItem displaySumOfFirstDigits= new MenuItem("Display the sum of the first digits of three numbers ", new DisplaySumOfFirstDigitsAction());
+        MenuContext.getCurrentMenu().getItems().add(displaySumOfFirstDigits);
+
+        MenuItem dispalyDifferenceBetweenNumbers = new MenuItem("Display difference between the first two numbers and the third number ", new DisplayDiffirenceBetweenNumbersAction());
+        MenuContext.getCurrentMenu().getItems().add(dispalyDifferenceBetweenNumbers);
+
+        MenuItem displaySumOfDigits = new MenuItem("Display sum of digits in random number ", new DisplaySumOfDigitsAction());
+        MenuContext.getCurrentMenu().getItems().add(displaySumOfDigits);
     }
 }
 
