ee6addb79117bdc8bad12a66141c673f7be11af5
baddreammello <andrej.czimbalist@inbox.ru>
2021-04-10 20:39:39 +0300

    Add:  import service.*

--- src/eu/senla/JavaLaboratory3/actions/EndTaskTwoAction.java
+++ src/eu/senla/JavaLaboratory3/actions/EndTaskTwoAction.java
@@ -1,8 +1,6 @@
 package eu.senla.JavaLaboratory3.actions;
 
-import eu.senla.JavaLaboratory3.context.Context;
-import eu.senla.JavaLaboratory3.services.MenuService;
-import eu.senla.JavaLaboratory3.services.TaskTwoService;
+import eu.senla.JavaLaboratory3.services.*;
 
 public class EndTaskTwoAction implements Action {
     @Override
--- src/eu/senla/JavaLaboratory3/actions/ToTaskTwoMenuAction.java
+++ src/eu/senla/JavaLaboratory3/actions/ToTaskTwoMenuAction.java
@@ -1,7 +1,6 @@
 package eu.senla.JavaLaboratory3.actions;
 
-import eu.senla.JavaLaboratory3.services.MenuService;
-import eu.senla.JavaLaboratory3.services.TaskTwoService;
+import eu.senla.JavaLaboratory3.services.*;
 
 public class ToTaskTwoMenuAction implements Action {
     @Override
